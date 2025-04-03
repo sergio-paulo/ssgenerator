69c1c65f2a147a585a0294c2a7308d811106e215
sergio.paulo <sergio.e.r.c.paulo@gmail.com>
2025-04-03 01:03:59 +0100

    D1: changed LeafNode to htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
@@ -36,4 +36,21 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode("a", "backend", None, {"href": "www.boot.dev"})
         self.assertEqual(node.props_to_html(), ' href="www.boot.dev"')
-        
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+            node = LeafNode("p", "Hello, world!")
+            self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+            
+    def test_leaf_to_html_a(self):
+            node = LeafNode("a", "Click me!", {"href": "https://www.gogole.com"})
+            self.assertEqual(node.to_html(), '<a href="https://www.gogole.com">Click me!</a>')
+            
+    def test_eq(self):
+            node = LeafNode("p", "Hello, world!")
+            node2 = LeafNode("p", "Hello, world!")
+            self.assertEqual(node, node2)
+            
+    def test_repr(self):
+            node = LeafNode("p", "Hello, world!")
+            self.assertEqual(repr(node), "LeafNode(p, Hello, world!, None)")
